ef59c2dab3e23ff93cf386af4d2e68afd278682e
albinanil45 <albinanilkumar45@gmail.com>
2025-04-17 20:31:55 +0530

    changed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4157181918"
+    serviceWorkerVersion: "474952597"
   }
 });
